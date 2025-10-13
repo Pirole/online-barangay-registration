f1992a27607e846a14c5a49d9b84acb2ebff62d9
Ethan <ethangarzo901@gmail.com>
2025-10-13 20:43:44 +0800

    Update

--- online-barangay-registration-backend/src/routes/registrations.ts
+++ online-barangay-registration-backend/src/routes/registrations.ts
@@ -12,7 +12,7 @@ const router = Router();
 router.post(
   '/',
   upload.single('photo'),
-  validateRequest(RegisterSchema),
+  //validateRequest(RegisterSchema),
   registrationsController.createRegistration
 );
 
